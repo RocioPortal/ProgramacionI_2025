f3fb81b537f233bb870a2fed10362bf9af660edd
Rocio Portal <p.portal@alumno.um.edu.ar>
2025-04-10 11:44:12 -0300

    agregando bd

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -1,19 +1,36 @@
 from flask import Flask
 from dotenv import load_dotenv
 
+
 from flask_restful import Api
 
-import main.resources as resources
+import os
+
+from flask_sqlalchemy import SQLAlchemy 
+
 
 #Inicializamos restful
 api = Api()
+
+#Inicializar sqlalchemy
+db = SQLAlchemy()
 
 def create_app():
     #Inicializar flask
     app = Flask(__name__)
     #cargamos variables de entorno
     load_dotenv()
+
+    if not os.path.exists(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')):
+        os.mknod(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME'))
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
+    db.init_app(app)
        
+
+    import main.resources as resources
+    
     #cargar los recursos
     api.add_resource(resources.UsuarioResource, '/usuario/<int:id>')
     api.add_resource(resources.UsuariosResource, '/usuarios')
