7c8358c5715b5907ef00b713eadb63c119e62c85
MelinaGomezTorres <mat.gomez@alumno.um.edu.ar>
2025-04-17 16:34:16 -0300

    Clase usuario/os

--- backend/main/models/usuarios.py
+++ backend/main/models/usuarios.py
@@ -7,12 +7,33 @@ class Usuario(db.Model):
     rol = db.Column(db.String(50), nullable=False)
     estado = db.Column(db.String(50), nullable=False)
 
+    pedidos = db.relationship("Pedido", back_populates="usuario",cascade="all, delete-orphan")
+    notificaciones = db.relationship('Notificacion', back_populates='usuario', cascade="all, delete", single_parent=True)
+    valoraciones = db.relationship('Valoracion', back_populates='usuario', cascade="all, delete", single_parent=True)
+
     def to_json(self):
         return {
             'id': self.id,
             'nombre': self.nombre,
             'rol': self.rol,
             'estado': self.estado
+        }
+    
+    def to_json_complete(self):
+        return {
+            'id': self.id,
+            'nombre': self.nombre,
+            'rol': self.rol,
+            'estado': self.estado,
+            'pedidos': [pedido.to_json() for pedido in self.pedidos],
+            'valoraciones': [valoracion.to_json() for valoracion in self.valoraciones],
+            'notificaciones': [notificacion.to_json() for notificacion in self.notificaciones]
+        }
+
+    def to_json_short(self):
+        return {
+            'id': self.id,
+            'nombre': self.nombre
         }
     
     @staticmethod
